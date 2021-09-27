9f5cabc53750909d2e6ca83de3b595308c560bcf
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-27 09:10:57 -0600

    basic handler to test was toolkit

--- src/main/java/com/revature/postComment/postCommentHandler.java
+++ src/main/java/com/revature/postComment/postCommentHandler.java
@@ -1,5 +1,24 @@
 package com.revature.postComment;
 
-public class postCommentHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class postCommentHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+
+        LambdaLogger logger = context.getLogger();
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+
+        logger.log("hey look, it worked!");
+
+        response.setStatusCode(418);
+
+        return response;
+    }
     // meaningless
+
 }
