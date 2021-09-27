aed1f24c8fdb444d57c4fd7adbb2cf537e5e1625
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-27 10:13:52 -0600

    set date_created if it isn't set already

--- src/main/java/com/revature/postComment/PostCommentService.java
+++ src/main/java/com/revature/postComment/PostCommentService.java
@@ -7,6 +7,9 @@ public class PostCommentService {
     private NodeRepository nodeRepo = new NodeRepository();
 
     public boolean addComment(Comment comment) {
+        if (comment.getDate_created().equals(null) || comment.getDate_created().length() != 26) {
+            comment.setDate_created(LocalDateTime.now().toString());
+        }
         boolean valid = isValid(comment);
         if (valid) {
             nodeRepo.addComment(comment);
