e81ea9715955d98827366cd0f4c0c6ddc5604947
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-27 10:09:44 -0600

    validation, fun times

--- src/main/java/com/revature/postComment/PostCommentService.java
+++ src/main/java/com/revature/postComment/PostCommentService.java
@@ -1,2 +1,27 @@
-package com.revature.postComment;public class PostCommentService {
+package com.revature.postComment;
+
+import java.time.LocalDateTime;
+
+public class PostCommentService {
+
+    private NodeRepository nodeRepo = new NodeRepository();
+
+    public boolean addComment(Comment comment) {
+        boolean valid = isValid(comment);
+        if (valid) {
+            nodeRepo.addComment(comment);
+        }
+        return valid;
+    }
+
+    public boolean isValid(Comment comment) {
+        LocalDateTime commentDate = LocalDateTime.parse(comment.getDate_created());
+
+        if (commentDate.isBefore(LocalDateTime.now().minusMinutes(10))) {
+            // if comment isn't new (within ten minutes of .now)
+            return false;
+        }
+        return true;
+    }
+
 }
