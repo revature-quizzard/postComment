45849782a46b5a66b42e1a277c612258df39b4d2
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-27 10:29:28 -0600

    wrote handler logic for post

--- src/main/java/com/revature/postComment/postCommentHandler.java
+++ src/main/java/com/revature/postComment/postCommentHandler.java
@@ -5,8 +5,14 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class postCommentHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private static final Gson mapper = new GsonBuilder().setPrettyPrinting().create();
+    private final PostCommentService service = new PostCommentService();
+
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
 
@@ -15,10 +21,17 @@ public class postCommentHandler implements RequestHandler<APIGatewayProxyRequest
 
         logger.log("hey look, it worked!");
 
-        response.setStatusCode(418);
+        boolean saved = service.addComment(mapper.fromJson(requestEvent.getBody(), Comment.class));
 
-        return response;
+        if (saved) {
+            response.setStatusCode(201);
+
+            return response;
+        } else {
+            response.setStatusCode(400);
+
+            return response;
+        }
     }
-    // meaningless
 
 }
