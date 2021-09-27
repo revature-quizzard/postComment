c39f38c58cab713b55121270d84d04416261fe7e
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-27 09:49:35 -0600

    yay lombok

--- src/main/java/com/revature/postComment/Comment.java
+++ src/main/java/com/revature/postComment/Comment.java
@@ -1,4 +1,17 @@
 package com.revature.postComment;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class Comment {
+
+    private String id;
+    private int date_created;
+    private List<String> ancestors;
+    private String description;
+    private String parent;
+    private String subject;
+
 }
