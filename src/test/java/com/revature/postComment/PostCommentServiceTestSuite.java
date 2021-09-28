340d8789bb9151c231ccf6c49806b2ed4cb7c7e4
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-28 07:56:09 -0600

    setup and cleanup

--- src/test/java/com/revature/postComment/PostCommentServiceTestSuite.java
+++ src/test/java/com/revature/postComment/PostCommentServiceTestSuite.java
@@ -1,4 +1,19 @@
 package com.revature.postComment;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 public class PostCommentServiceTestSuite {
+
+    PostCommentService sut;
+
+    @BeforeEach
+    public void caseSetUp() {
+        sut = new PostCommentService();
+    }
+
+    @AfterEach
+    public void caseCleanUp() {
+        sut = null;
+    }
 }
