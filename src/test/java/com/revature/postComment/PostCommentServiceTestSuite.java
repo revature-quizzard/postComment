09cc7953903713bb0a6167a25d0bfeffe1704c83
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-28 09:43:24 -0600

    mocked NodeRepository

--- src/test/java/com/revature/postComment/PostCommentServiceTestSuite.java
+++ src/test/java/com/revature/postComment/PostCommentServiceTestSuite.java
@@ -2,18 +2,24 @@ package com.revature.postComment;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import static org.mockito.Mockito.*;
 
 public class PostCommentServiceTestSuite {
 
     PostCommentService sut;
+    NodeRepository mockNodeRepo;
 
     @BeforeEach
     public void caseSetUp() {
-        sut = new PostCommentService();
+        mockNodeRepo = mock(NodeRepository.class);
+        sut = new PostCommentService(mockNodeRepo);
     }
 
     @AfterEach
     public void caseCleanUp() {
         sut = null;
+        reset(mockNodeRepo);
     }
+
+
 }
