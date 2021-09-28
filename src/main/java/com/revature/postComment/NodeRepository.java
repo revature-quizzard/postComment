6c3171eee2db4853cc55016745b851f55a22f79e
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-28 10:34:44 -0600

    queries are fun, right?

--- src/main/java/com/revature/postComment/NodeRepository.java
+++ src/main/java/com/revature/postComment/NodeRepository.java
@@ -2,6 +2,7 @@ package com.revature.postComment;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 
 public class NodeRepository {
 
@@ -13,5 +14,11 @@ public class NodeRepository {
 
     public void addComment(Comment comment) {
         dbmapper.save(comment);
+    }
+
+    public Comment getThread(String id) {
+        Comment queryItem = Comment.builder().id(id).build();
+
+        return dbmapper.query(Comment.class,new DynamoDBQueryExpression<Comment>().withHashKeyValues(queryItem)).get(0);
     }
 }
