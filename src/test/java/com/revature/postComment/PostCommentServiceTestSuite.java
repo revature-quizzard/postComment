3c1e3bc6b25fab72a44f204d9d55eaf6b4495c4a
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-28 10:09:39 -0600

    wrote successful test case

--- src/test/java/com/revature/postComment/PostCommentServiceTestSuite.java
+++ src/test/java/com/revature/postComment/PostCommentServiceTestSuite.java
@@ -2,7 +2,14 @@ package com.revature.postComment;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PostCommentServiceTestSuite {
 
@@ -19,6 +26,43 @@ public class PostCommentServiceTestSuite {
     public void caseCleanUp() {
         sut = null;
         reset(mockNodeRepo);
+    }
+
+    @Test
+    public void addComment_returnsSuccessfully_givenValidInformation() {
+        // Arrange
+        Comment mockComment = Comment.builder()
+                                     .id("12345")
+                                     .parent("threadId")
+                                     .ancestors(new ArrayList<>(Arrays.asList("subforumId","threadId")))
+                                     .description("Description goes here")
+                                     .owner("user")
+                                     .build();
+        Comment mockParentThread = Comment.builder()
+                                          .id("threadId")
+                                          .parent("subforumId")
+                                          .ancestors(new ArrayList<>(Arrays.asList("subforumId")))
+                                          .description("Thread description")
+                                          .child_count(2)
+                                          .date_created(LocalDateTime.now().minusDays(12).toString())
+                                          .subject("Core Java")
+                                          .owner("user2")
+                                          .build();
+        doNothing().when(mockNodeRepo).addComment(any());
+        when(mockNodeRepo.getThread(any())).thenReturn(mockParentThread);
+        doNothing().when(mockNodeRepo).updateChild_count(any());
+
+        // Act
+        boolean result = sut.addComment(mockComment);
+
+        // Assert
+        verify(mockNodeRepo, times(1)).getThread("threadId");
+        verify(mockNodeRepo, times(1)).addComment(any());
+        verify(mockNodeRepo, times(1)).updateChild_count(any());
+        assertEquals(mockParentThread.getChild_count(), 3);
+        assertNotNull(mockComment.getDate_created());
+        assertTrue(result);
+
     }
 
 
